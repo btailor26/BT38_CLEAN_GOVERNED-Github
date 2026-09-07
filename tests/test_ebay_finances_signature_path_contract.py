2f1cde1a3c2ec2577c7f5818265e893b68974370
btailor26 <bhavtee@gmail.com>
2026-09-07 17:51:39 +0100

    Add eBay Finances signature path contract

---
+++ tests/test_ebay_finances_signature_path_contract.py
@@ -0,0 +1,15 @@
+from services.governed_ebay_shipping_label_finance import _signature_path
+
+
+def test_ebay_finances_signature_path_excludes_query_string():
+    url = (
+        "https://api.ebay.com/sell/finances/v1/transaction"
+        "?filter=transactionType%3A%7BSHIPPING_LABEL%7D%2CorderId%3A%7B27-15097-79712%7D"
+        "&limit=100"
+    )
+
+    assert _signature_path(url) == "/sell/finances/v1/transaction"
+
+
+def test_ebay_finances_signature_path_defaults_to_root():
+    assert _signature_path("https://api.ebay.com") == "/"
